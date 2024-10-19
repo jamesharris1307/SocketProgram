c466df19802bf58a90e03f7ad9f9b1f8a2760d23
James Harris <jamesharris1307@outlook.com>
2024-10-19 13:54:14 +0100

    lol

---
+++ src/Client.java
@@ -0,0 +1,65 @@
+// Imports
+import java.io.*;
+import java.net.*;
+
+// Client Class
+public class Client {
+    // Initialize socket and input output streams
+    private Socket socket = null;
+    private DataInputStream input = null;
+    private DataOutputStream out = null;
+
+    // Constructor to put ip address and port
+    public Client(String address, int port)
+    {
+        // Establish Connection
+        try {
+            socket = new Socket(address, port);
+            System.out.println("Connected");
+
+            // Takes input from terminal
+            input = new DataInputStream(System.in);
+
+            // Sends output to the socket
+            out = new DataOutputStream(
+                    socket.getOutputStream());
+        }
+        catch (UnknownHostException u) {
+            System.out.println(u);
+            return;
+        }
+        catch (IOException i) {
+            System.out.println(i);
+            return;
+        }
+
+        // String to read message from input
+        String line = "";
+
+        // Keep reading until "Over" is input
+        while (!line.equals("Over")) {
+        try {
+            line = input.readLine();
+            out.writeUTF(line);
+        }
+        catch (IOException i) {
+            System.out.println(i);
+        }
+    }
+
+    // Close the connection
+        try {
+            input.close();
+            out.close();
+            socket.close();
+        }
+        catch (IOException i) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String args[])
+    {
+        Client client = new Client("127.0.0.1", 5000);
+    }
+}
---
+++ src/Server.java
@@ -0,0 +1,59 @@
+// Imports
+import java.net.*;
+import java.io.*;
+
+public class Server
+{
+    // Initialize socket and input stream
+    private Socket socket = null;
+    private ServerSocket server = null;
+    private DataInputStream in = null;
+
+    // Constructor with port
+    public Server(int port)
+    {
+        // Starts server and waits for a connection
+        try
+        {
+            server = new ServerSocket(port);
+            System.out.println("Server started");
+
+            System.out.println("Waiting for client . . .");
+
+            socket = server.accept();
+            System.out.println("Client accepted");
+
+            // Takes input from the client socket
+            in = new DataInputStream(
+                    new BufferedInputStream(socket.getInputStream()));
+
+            String line = "";
+
+            // Reads message from client until "Over" is sent
+            while (!line.equals("Over"))
+            {
+                try
+                {
+                    line = in.readUTF();
+                    System.out.println(line);
+                } catch (IOException i) {
+                    System.out.println(i);
+                }
+            }
+            System.out.println("Closing Connection");
+
+            // Close Connection
+            socket.close();
+            in.close();
+        }
+        catch(IOException i)
+        {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String args[])
+    {
+        Server server = new Server(5000);
+    }
+}
